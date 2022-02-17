45715e533886b0887b8dd8ffcc73c53c1a61473b
biglimp <biglimp@users.noreply.github.com>
2022-02-17 10:53:17 +0100

    Update QGIS3_shell.bat
    To work with Python 3.9

--- CodeAndStuff/QGIS3_shell.bat
+++ CodeAndStuff/QGIS3_shell.bat
@@ -1,20 +1,16 @@
-SET OSGEO4W_ROOT=C:\OSGeo4W64
+SET OSGEO4W_ROOT=C:\OSGeo4W
 call %OSGEO4W_ROOT%\bin\o4w_env.bat"
-call qt5_env.bat
-call py3_env.bat
 
+@echo off
 path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis
 set GDAL_FILENAME_IS_UTF8=YES
 rem Set VSI cache to be used as buffer, see #6448
-rem set VSI_CACHE=TRUE
-rem set VSI_CACHE_SIZE=1000000
-set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-dev\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
+set VSI_CACHE=TRUE
+set VSI_CACHE_SIZE=1000000
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis\python;%PYTHONPATH%
-set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python37
-rem "%PYTHONHOME%\python" %*
 
 cdm.exe
 @echo on
 @if [%1]==[] (echo run o-help for a list of available commands & cmd.exe /k) else (cmd /c "%*")
-
